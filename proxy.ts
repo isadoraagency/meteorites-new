8a2e8ecb44c8c5e37cd533bef41696a15c43b685
Alejandro Ariel Gerbec <gerbec.alejandro@gmail.com>
2026-07-06 14:54:25 -0300

    fix: proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const isStoryblokPreview =
     request.nextUrl.searchParams.has("_storyblok") ||
     request.nextUrl.searchParams.has("_storyblok_tk");
